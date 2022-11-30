6293acde73e2aae4886e44ef2c12b6e6ece3d7ca
AristideTizi <57866006+AristideTizi@users.noreply.github.com>
2022-11-30 23:54:01 +0100

    Templates creation

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
 
-# Create your views here.
+def hello(request):
+    bands = Band.objects.all()
+    listings = Listing.objects.all()
+    return render(
+        request,
+        'listings/hello.html',
+        {'bands': bands}
+        )
+
+def about(request):
+    return render(
+        request,
+        'listings/about.html',
+    )
+
+def contact(request):
+    return render(
+        request,
+        'listings/contact.html',
+    )
+
+def listings(request):
+    listings = Listing.objects.all()
+    return render(
+        request,
+        'listings/listings.html',
+        {'listings': listings}
+    )
